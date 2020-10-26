d2c9260c3308375dd87a2cad3769fd095b012c9a
John Wasielewicz <wazjohn@yahoo.com>
2020-10-26 17:23:14 -0500

    Persisting employers and skills.

--- queries.sql
+++ queries.sql
@@ -1,7 +1,7 @@
 ## Part 1: Test it with SQL
 SELECT COLUMN_NAME, DATA_TYPE
 FROM INFORMATION_SCHEMA.COLUMNS
-WHERE table_name = 'job';
+WHERE table_name = "job";
 
 id INT
 employer VARCHAR(255)
@@ -9,6 +9,9 @@ name VARCHAR(255)
 skills VARCHAR(255)
 
 ## Part 2: Test it with SQL
+SELECT name
+FROM employer
+WHERE location = "St. Louis";
 
 ## Part 3: Test it with SQL
 
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,6 +1,23 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    @NotBlank(message = "Location is required")
+    @Size(min = 4, max = 100, message = "Location must be between 4 and 100 characters")
+    private String location;
 
+    public Employer() {}
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }

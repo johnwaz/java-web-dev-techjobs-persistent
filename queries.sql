b5112028942809c943b9ca13818f5921daa3f17a
John Wasielewicz <wazjohn@yahoo.com>
2020-10-26 14:23:21 -0500

    SQL task Part 1

--- queries.sql
+++ queries.sql
@@ -1,4 +1,12 @@
 ## Part 1: Test it with SQL
+SELECT COLUMN_NAME, DATA_TYPE
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE table_name = 'job';
+
+id INT
+employer VARCHAR(255)
+name VARCHAR(255)
+skills VARCHAR(255)
 
 ## Part 2: Test it with SQL
 
